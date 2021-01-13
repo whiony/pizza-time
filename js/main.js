5b9296fca19cc886c37c962e955f4d13166e2104
YanaWhitte <shiro.kirio@gmail.com>
2021-01-13 19:01:51 +0200

    Finished optimization, added favicon etc

--- js/main.js
+++ js/main.js
@@ -349,31 +349,56 @@ myLib.body.addEventListener('click', function(e) {
 
 // map
 ;(function() {
-  if (typeof ymaps === 'undefined') {
-    return;
-  }
+  var sectionContacts = document.querySelector(".section-contacts");
 
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('ymap', {
-            center: [55.794887, 37.712812],
-            zoom: 16
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+  var ymapInit = function() {
+    if (typeof ymaps === 'undefined') {
+      return;
+    }
+  
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map('ymap', {
+              center: [55.794887, 37.712812],
+              zoom: 16
+          }, {
+              searchControlProvider: 'yandex#search'
+          }),
+  
+          myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+              balloonContent: 'г. Москва, Преображенская площадь, 8'
+          }, {
+              iconLayout: 'default#image',
+              iconImageHref: '../img/common/marker.svg',
+              iconImageSize: [40, 63.2],
+              iconImageOffset: [-5, -38]
+          });
+  
+      myMap.geoObjects.add(myPlacemark);
+  
+      myMap.behaviors.disable('scrollZoom');
+    });
+  };
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            balloonContent: 'г. Москва, Преображенская площадь, 8'
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: '../img/common/marker.svg',
-            iconImageSize: [40, 63.2],
-            iconImageOffset: [-5, -38]
-        });
+  var ymapLoad = function() {
+    var script = document.createElement('script');
+    script.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
+    myLib.body.appendChild(script);
+    script.onload = ymapInit;
+  };
 
-    myMap.geoObjects.add(myPlacemark);
+  var checkYmapInit = function() {
+    var sectionContactsTop = sectionContacts.getBoundingClientRect().top;
+    var scrollTop = window.pageYOffset;
+    var sectionContactsOffsetTop = scrollTop + sectionContactsTop;
 
-    myMap.behaviors.disable('scrollZoom');
-  });
+    if (scrollTop + window.innerHeight > sectionContactsOffsetTop) {
+      ymapLoad();
+      window.removeEventListener('scroll', checkYmapInit);
+    }
+  };
+
+  window.addEventListener('scroll', checkYmapInit);
+  checkYmapInit();
 })();
 // map
 
